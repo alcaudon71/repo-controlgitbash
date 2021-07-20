ab938398d51ecb15b63da332a4237d5b3e32b0da
xjossanc <jose.sanchez.tamborero@itbo-ericsson.com>
2021-07-20 16:42:32 +0200

    Commit inicial del proyecto

---
+++ src/main/java/com/universo/repositorio/AppGit.java
@@ -0,0 +1,14 @@
+// **************************************************
+package com.universo.repositorio;
+
+public class AppGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Creacion de un repositorio Git desde Git Bash");
+		
+	}
+
+}
+//**************************************************
